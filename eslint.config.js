355f17aa8dd3e44be7d0d80bc8819971c4cd2e2f
GaeMaz <gaetano.mazzarella@haufe-lexware.com>
2025-08-30 23:12:53 +0200

    Debugging Eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,12 @@
 import astro from "eslint-plugin-astro";
-import tseslint from "@typescript-eslint/eslint-plugin";
-import tsparser from "@typescript-eslint/parser";
 
 export default [
-  {
-    files: ["**/*.astro"],
-    plugins: { astro },
-    languageOptions: {
-      parser: tsparser,
-    },
-    rules: {
-      ...astro.configs.recommended.rules,
-    },
-  },
+  // Add the Astro configuration
+  ...astro.configs.recommended,
   {
     files: ["**/*.ts", "**/*.js"],
-    plugins: { "@typescript-eslint": tseslint },
-    languageOptions: {
-      parser: tsparser,
-    },
     rules: {
-      ...tseslint.configs.recommended.rules,
+      // Add any additional TypeScript/JavaScript rules here
     },
   },
 ];
